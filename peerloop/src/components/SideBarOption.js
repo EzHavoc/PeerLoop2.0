3a03ae6f38a38811a0d19f193ec3ecf5a4f7d7cc
EzHavoc <kanishksingh778@gmail.com>
2024-11-28 19:22:09 +0530

    React-Redux

--- peerloop/src/components/SideBarOption.js
+++ peerloop/src/components/SideBarOption.js
@@ -1,43 +1,42 @@
 import React from 'react';
+import { useDispatch } from "react-redux";
 import styled from 'styled-components';
-import {db} from "../firebase";
 import { collection, addDoc } from "firebase/firestore";
-function SideBarOption({ Icon, title, addChannelOption}) { // Use consistent "title"
-  
+import { db } from "../firebase";
+import { enterRoom } from "../features/appSlice";
+
+function SideBarOption({ Icon, title, addChannelOption, id }) {
+  const dispatch = useDispatch();
+
   const addChannel = async () => {
-        const channelName = prompt("Please enter the channel name!");
-      
-        if (channelName) {
-          try {
-            // Reference the 'rooms' collection
-            const roomsCollection = collection(db, "rooms");
-      
-            // Add a new channel
-            const docRef = await addDoc(roomsCollection, {
-              name: channelName,
-            });
-      
-            console.log("Channel added with ID:", docRef.id);
-          } catch (error) {
-            console.error("Error adding channel:", error);
-          }
-        } else {
-          console.log("Channel name is required!");
-        }
-      };
-      
-      const selectChannel = (channelId) => {
-        // Logic for handling channel selection
-        if (channelId) {
-          console.log(`Channel selected with ID: ${channelId}`);
-          // Add additional code to navigate to the channel or display its content
-        } else {
-          console.error("Invalid channel ID.");
-        }
-      };
+    const channelName = prompt("Please enter the channel name!");
+
+    if (channelName) {
+      try {
+        const collectionRef = collection(db, "rooms");
+        await addDoc(collectionRef, { name: channelName });
+        console.log("Channel added successfully!");
+      } catch (error) {
+        console.error("Error adding channel: ", error);
+      }
+    } else {
+      console.log("Channel name is required!");
+    }
+  };
+
+  const selectChannel = (channelId) => {
+    if (channelId) {
+      dispatch(
+        enterRoom({
+          roomId: channelId,
+        })
+      );
+    }
+  };
+
   return (
     <SideBarOptionContainer
-    onClick={addChannelOption ? addChannel : selectChannel}
+      onClick={addChannelOption ? addChannel : () => selectChannel(id)}
     >
       {Icon && <Icon fontSize="small" style={{ padding: 10 }} />}
       {Icon ? (
@@ -54,19 +53,22 @@ function SideBarOption({ Icon, title, addChannelOption}) { // Use consistent "ti
 export default SideBarOption;
 
 const SideBarOptionContainer = styled.div`
- display: flex;
+  display: flex;
   font-size: 12px;
   align-items: center;
   padding-left: 2px;
   cursor: pointer;
-  :hover{
+
+  :hover {
     opacity: 0.9;
     background-color: #340e36;
   }
-  > h3{
+
+  > h3 {
     font-weight: 500;
   }
-  >h3>span{
+
+  > h3 > span {
     padding: 15px;
   }
 
@@ -75,7 +77,7 @@ const SideBarOptionContainer = styled.div`
   }
 `;
 
-const SideBarOptionChannel = styled.div`
+const SideBarOptionChannel = styled.h3`
   padding: 10px 10px;
   font-weight: 700;
 `;
