c7e38100439e70b5bbacd99b7d0b88fc6463634b
KushagraShukla30 <1ds23cd027@dsce.edu.in>
2024-11-30 19:42:35 +0530

    final commit

--- peerloop/src/App.js
+++ peerloop/src/App.js
@@ -2,22 +2,49 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from "./components/Header";
 import SideBar from "./components/SideBar";
-import Chat from "./components/Chat"; // Import your Chat component
+import Chat from "./components/Chat";
 import styled from 'styled-components';
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "./firebase";
+import Login from './components/Login';
+import Spinner from 'react-spinkit';
 
 function App() {
+  const [user, loading] = useAuthState(auth);
+
+  if(loading){
+    return(
+      <AppLoading>
+        <AppLoadingContents>
+        <img src="/PeerLoopLogo.png" alt="PeerLoop Logo" height="150" />
+
+        <Spinner
+          name="ball-spin-fade-loader"
+          color="orange"
+          fadeIn="none"
+        />
+        </AppLoadingContents>
+      </AppLoading>
+    )
+  }
+
   return (
     <div className="app">
       <Router>
-        <Header /> {/* Always visible */}
-        <AppBody>
-          <SideBar /> {/* Always visible */}
-          <Routes>
-            {/* Define routes as needed */}
-            <Route path="/" element={<Chat />} /> {/* Chat is displayed on the root path */}
-            <Route path="/chat" element={<Chat />} /> {/* Example route for the chat */}
-          </Routes>
-        </AppBody>
+        {!user ? (
+          <Login />
+        ) : (
+          <>
+            <Header userPhoto={user?.photoURL} userName={user?.displayName} /> {/* Pass profile details */}
+            <AppBody>
+              <SideBar />
+              <Routes>
+                <Route path="/" element={<Chat />} />
+                <Route path="/chat" element={<Chat />} />
+              </Routes>
+            </AppBody>
+          </>
+        )}
       </Router>
     </div>
   );
@@ -25,8 +52,29 @@ function App() {
 
 export default App;
 
-// Styled Components for layout
+const AppLoading = styled.div`
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  width: 100%;
+`;
+
+const AppLoadingContents = styled.div`
+text-align: center;
+  padding-bottom: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  >img{
+    height: 100px;
+    padding: 20px;
+    margin-bottom: 40px;
+  }
+`;
+
 const AppBody = styled.div`
   display: flex;
-  height: 100vh; /* Takes up remaining space below the header */
+  height: 100vh;
 `;
